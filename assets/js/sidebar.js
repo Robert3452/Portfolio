65c73242773b90f0caefe6f3999e0395d1d483c6
Robert Meza Garcia <romega_2011@hotmail.com>
2020-08-28 11:22:38 -0500

    adding dashboard

---
+++ assets/js/sidebar.js
@@ -0,0 +1,8 @@
+var btn_sidebar = document.querySelector('.btn-sidebar');
+var sidebarBody = document.querySelector('.sidebar-content');
+var sidebar = document.querySelector('.sidebar');
+
+btn_sidebar.addEventListener('click', () => {
+    sidebarBody.classList.toggle("sidebar-active");
+    sidebar.classList.toggle("toggle")
+})
